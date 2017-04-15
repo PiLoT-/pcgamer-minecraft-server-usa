4a1555bb110a8e13b30d139fb07eb5233aa52e64
Queue <TheQueue@gmail.com>
2017-04-15 09:19:24 -0700

    Delete rtoolkit.sh

--- server/rtoolkit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USER=user
-PASS=pass
-
-##YOU SHOULD NOT NEED TO EDIT ANYTHING BELOW THIS LINE##
-DIRECTORY=$(cd "`dirname "$0"`" && pwd)
-(cd "$DIRECTORY"; java -Xmx30M -Xms30M -XX:MaxPermSize=40M -jar Minecraft_RKit.jar ${USER}:${PASS})
